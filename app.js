c496002e53291def50f5fafed1c37b11a1d85e6a
stromcer <dannymm12@hotmail.com>
2023-02-15 21:25:23 +0100

    Finalizado el modal

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import { Body } from "./modules/components/body.js";
 import { NavBar } from "./modules/components/navBar.js";
 import { todoCard } from "./modules/components/todoCard.js";
 import { NewTaskModal } from "./modules/components/newTaskModal.js";
-import { NewTask } from "./modules/components/newTask.js";
+import { NewTask } from "./modules/utils/newTask.js";
 
 function render(){
     Body({todoCard});
